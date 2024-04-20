2a091b23db20597f91fbe741b37de2485d77f1ab
Blossom <mariajesuss2424@gmail.com>
2024-04-19 22:39:33 -0400

    structure pending

---
+++ assets/js/productos.js
@@ -0,0 +1,17 @@
+productos = [
+    {
+        name: "AIR JORDAN LEGO VERSION",
+        stock: 10,
+        marca: "Nike",
+        modelo: "AIR JORDAN1 COLOR CHICAGO",
+        descripcion: "AIR JORDAN LEGO VERSION",
+        precio: 10000,
+        fotos: {
+            foto1: "./assets/img/producto1.jpg"
+        }
+    }
+]
+
+
+
+// console.log(productos.fotos.foto1);
